501b6791067afca4b749e098345f8e20d3abd595
Hemdan <hemdanGit@gmail.com>
2023-07-24 03:02:55 +0300

    first commit

---
+++ lib/cubit/shop_cubit.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'shop_state.dart';
+
+class ShopCubit extends Cubit<ShopState> {
+  ShopCubit() : super(ShopInitial());
+  static ShopCubit get(context) => BlocProvider.of(context);
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+
+  bool isSecure = false;
+  showPassword() {
+    isSecure = !isSecure;
+    emit(ShowPassword());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shop_app/Screens/OnboardingScreen/OnboardingScreen.dart';
+import 'package:shop_app/cubit/bloc_observer.dart';
+import 'package:shop_app/cubit/shop_cubit.dart';
+
+void main() {
+  Bloc.observer = MyBlocObserver();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ShopCubit(),
+      child: ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+            useMaterial3: true,
+          ),
+          home: OnboardingScreen(),
+        ),
+      ),
+    );
+  }
+}
