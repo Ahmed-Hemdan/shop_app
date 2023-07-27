affab2aa8185bece243f45f8deb6f1cb1a940b33
Hemdan <ahmedhemdan.dev@gmail.com>
2023-07-27 18:03:14 +0300

    try to make MasonryGridView

--- lib/Screens/Home/HomeScreen.dart
+++ lib/Screens/Home/HomeScreen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:shop_app/Components/Components.dart';
+import 'package:shop_app/Models/HomeModel.dart';
 import 'package:shop_app/cubit/shop_cubit.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,20 +11,47 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            ShopCubit.get(context).getHomeData();
-          },
-          child: Text(
-            'try',
-            style: TextStyle(
-              fontSize: 20.sp,
-            ),
-          ),
-        ),
-      ),
+    return BlocConsumer<ShopCubit, ShopStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          body: ShopCubit.get(context).homeData == null
+              ? const Center(
+                  child: CircularProgressIndicator.adaptive(),
+                )
+              : Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      mySlider(ShopCubit.get(context).homeData),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                          child: MasonryGridView.builder(
+                            gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                            ),
+                            itemCount: ShopCubit.get(context).homeData!.data!.products!.length,
+                            physics: const BouncingScrollPhysics(),
+                            itemBuilder: (context, index) => Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: justForTry(
+                                context: context,
+                                model: ShopCubit.get(context).homeData,
+                                index: index,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+        );
+      },
     );
   }
 }
