a0f520140c2eecf9aa58454527f903b0d8d83bdb
Hemdan <hemdanGit@gmail.com>
2023-08-11 17:14:52 +0300

    add favorits screen and all functions

--- lib/Screens/Favorites/FavoritesScreen.dart
+++ lib/Screens/Favorites/FavoritesScreen.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/Components/Components.dart';
+import 'package:shop_app/cubit/shop_cubit.dart';
 
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Favorites Screen",
-        ),
-      ),
+    return BlocConsumer<ShopCubit, ShopStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return ShopCubit.get(context).favoritsData == null
+            ? const Center(child: CircularProgressIndicator.adaptive())
+            : RefreshIndicator(
+                onRefresh: () {
+                  ShopCubit.get(context).favoritsData = null;
+                 return ShopCubit.get(context).getFavoritsData();
+                },
+                child: ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: ShopCubit.get(context).favoritsData!.data!.data!.length,
+                  itemBuilder: (
+                    context,
+                    index,
+                  ) {
+                    return favoritsListItem(ShopCubit.get(context).favoritsData!, index , context);
+                  },
+                ),
+              );
+      },
     );
   }
 }
